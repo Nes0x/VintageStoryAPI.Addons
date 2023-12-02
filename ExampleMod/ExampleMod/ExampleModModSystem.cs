1d0c113f2cd0d29d58796d2a697b7e3f69b98773
Nes0x <przemek882@onet.pl>
2023-12-02 15:51:03 +0100

    Remove commented code

--- ExampleMod/ExampleMod/ExampleModModSystem.cs
+++ ExampleMod/ExampleMod/ExampleModModSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 using VintageStoryAPI.Addons.CommandHandler;
 
 namespace ExampleMod;
@@ -8,23 +9,13 @@ public class ExampleModModSystem : ModSystem
 {
     public override void StartClientSide(ICoreClientAPI api)
     {
-        // api.ChatCommands.Create("bim")
-        //     .HandleWith(_ => TextCommandResult.Success("bim"))
-        //     .WithArgs(new WordArgParser("word", true))
-        //     .BeginSubCommand("aha")
-        //     .HandleWith(_ => TextCommandResult.Success("aha"))
-        //     .EndSubCommand()
-        //     .BeginSubCommand("no")
-        //     .WithPreCondition(_ => TextCommandResult.Success("błąd"))
-        //     .WithPreCondition(_ => TextCommandResult.Error("błont"))
-        //     .HandleWith(_ => TextCommandResult.Success("no"));
         var commandHandler = new CommandHandler<ICoreClientAPI>(api);
         commandHandler.RegisterCommands();
     }
 
-    // public override void StartServerSide(ICoreServerAPI api)
-    // {
-    //     var commandHandler = new CommandHandler<ICoreServerAPI>(api);
-    //     commandHandler.RegisterCommands();
-    // }
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        var commandHandler = new CommandHandler<ICoreServerAPI>(api);
+        commandHandler.RegisterCommands();
+    }
 }
